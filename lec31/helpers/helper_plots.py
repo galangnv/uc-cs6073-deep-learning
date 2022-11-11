febd8628e8e13f86b97db63cfe9abdda6c21da7b
Christian Galang <nxvgalang@gmail.com>
2022-11-11 02:36:03 -0500

    Lec 31: Complete activity

--- lec31/helpers/helper_plots.py
+++ lec31/helpers/helper_plots.py
@@ -37,3 +37,36 @@ def plot_generated_images(
                 ax[i].imshow(curr_img)
             else:
                 ax[i].imshow(curr_img.view((image_height, image_width)), cmap='binary')
+
+def plot_modified_classes(
+    original,
+    diff,
+    diff_coefficients=(0., 0.5, 1., 1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5.),
+    decoding_fn=None,
+    device=None
+):
+    _, axes = plt.subplots(
+        nrows=1, ncols=len(diff_coefficients),
+        sharex=True, sharey=True,
+        figsize=(11, 1.5)
+    )
+
+    for i, alpha in enumerate(diff_coefficients):
+        transition = original + alpha * diff
+
+        if decoding_fn is not None:
+
+            with torch.no_grad():
+                if device is not None:
+                    transition = transition.to(device)
+                
+                transition = decoding_fn(transition).to(torch.device('cpu')).squeeze(0)
+        
+        if not alpha:
+            s = 'original'
+        else:
+            s = f'$\\alpha=${alpha}'
+
+        axes[i].set_title(s)
+        axes[i].imshow(transition.permute(1, 2, 0))
+        axes[i].axison = False
